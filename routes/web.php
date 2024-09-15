4e7cc252edd85da29b8b24e0278b0e6672664f09
resatyildiz <resat328@gmail.com>
2024-09-15 16:41:15 +0300

    fix: remove unused files

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::get('/', [MainController::class,'mainshow'])->name ('show');
